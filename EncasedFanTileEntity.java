d4babe5356d9bc355eba70932100b62f1ef4a9cc
dolemitesampson <101364381+dolemitesampson@users.noreply.github.com>
2022-07-11 15:20:44 -0500

    reverted the fan to a generator in create mod

---
+++ EncasedFanTileEntity.java
@@ -0,0 +1,221 @@
+package com.simibubi.create.content.contraptions.components.fan;
+
+import java.util.List;
+
+import javax.annotation.Nullable;
+import com.simibubi.create.AllBlocks;
+import com.simibubi.create.AllTags.AllBlockTags;
+import com.simibubi.create.content.contraptions.base.GeneratingKineticTileEntity;
+import com.simibubi.create.content.contraptions.processing.burner.BlazeBurnerBlock;
+import com.simibubi.create.content.contraptions.base.GeneratingKineticTileEntity;
+
+import com.simibubi.create.content.logistics.block.chute.ChuteTileEntity;
+import com.simibubi.create.foundation.advancement.AllAdvancements;
+import com.simibubi.create.foundation.config.AllConfigs;
+import com.simibubi.create.foundation.tileEntity.TileEntityBehaviour;
+
+import net.minecraft.MethodsReturnNonnullByDefault;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+
+@MethodsReturnNonnullByDefault
+public class EncasedFanTileEntity extends GeneratingKineticTileEntity implements IAirCurrentSource {
+
+	public AirCurrent airCurrent;
+	protected int airCurrentUpdateCooldown;
+	protected int entitySearchCooldown;
+	protected boolean isGenerator;
+	protected boolean updateAirFlow;
+	protected boolean updateGenerator;
+
+
+	public EncasedFanTileEntity(BlockEntityType<?> type, BlockPos pos, BlockState state) {
+		super(type, pos, state);
+		isGenerator = true;
+		airCurrent = new AirCurrent(this);
+		updateAirFlow = true;
+		updateGenerator = true;
+	}
+
+	@Override
+	public void addBehaviours(List<TileEntityBehaviour> behaviours) {
+		super.addBehaviours(behaviours);
+		registerAwardables(behaviours, AllAdvancements.ENCASED_FAN, AllAdvancements.FAN_PROCESSING);
+	}
+
+	@Override
+	protected void read(CompoundTag compound, boolean clientPacket) {
+		super.read(compound, clientPacket);
+		if (!wasMoved)
+			isGenerator = compound.getBoolean("Generating");
+		if (clientPacket)
+			airCurrent.rebuild();
+	}
+
+	@Override
+	public void write(CompoundTag compound, boolean clientPacket) {
+		compound.putBoolean("Generating", isGenerator);
+		super.write(compound, clientPacket);
+	}
+	@Override
+	public float calculateAddedStressCapacity() {
+		return lastCapacityProvided = (isGenerator ? super.calculateAddedStressCapacity() : 0);
+	}
+
+	@Override
+	public float calculateStressApplied() {
+		return isGenerator ? 0 : super.calculateStressApplied();
+	}
+
+	@Override
+	public float getGeneratedSpeed() {
+		return isGenerator ? AllConfigs.SERVER.kinetics.generatingFanSpeed.get() : 0;
+	}
+
+	public void queueGeneratorUpdate() {
+		updateGenerator = true;
+	}
+
+	public void updateGenerator() {
+		BlockState blockState = getBlockState();
+		boolean shouldGenerate = true;
+
+		if (!AllBlocks.ENCASED_FAN.has(blockState))
+			shouldGenerate = false;
+
+		if (shouldGenerate && blockState.getValue(EncasedFanBlock.FACING) != Direction.DOWN)
+			shouldGenerate = false;
+
+		if (shouldGenerate)
+			shouldGenerate = level != null && level.hasNeighborSignal(worldPosition)
+					&& level.isLoaded(worldPosition.below()) && blockBelowIsHot();
+
+		if (shouldGenerate == isGenerator)
+			return;
+		isGenerator = shouldGenerate;
+		updateGeneratedRotation();
+	}
+
+	public boolean blockBelowIsHot() {
+		if (level == null)
+			return false;
+		BlockState checkState = level.getBlockState(worldPosition.below());
+
+		if (!AllBlockTags.FAN_HEATERS.matches(checkState))
+			return false;
+
+		if (checkState.hasProperty(BlazeBurnerBlock.HEAT_LEVEL) && !checkState.getValue(BlazeBurnerBlock.HEAT_LEVEL)
+				.isAtLeast(BlazeBurnerBlock.HeatLevel.FADING))
+			return false;
+
+		if (checkState.hasProperty(BlockStateProperties.LIT) && !checkState.getValue(BlockStateProperties.LIT))
+			return false;
+
+		return true;
+	}
+
+	@Override
+	public AirCurrent getAirCurrent() {
+		return airCurrent;
+	}
+
+	@Nullable
+	@Override
+	public Level getAirCurrentWorld() {
+		return level;
+	}
+
+	@Override
+	public BlockPos getAirCurrentPos() {
+		return worldPosition;
+	}
+
+	@Override
+	public Direction getAirflowOriginSide() {
+		return this.getBlockState()
+			.getValue(EncasedFanBlock.FACING);
+	}
+
+	@Override
+	public Direction getAirFlowDirection() {
+		float speed = getSpeed();
+		if (speed == 0)
+			return null;
+		Direction facing = getBlockState().getValue(BlockStateProperties.FACING);
+		speed = convertToDirection(speed, facing);
+		return speed > 0 ? facing : facing.getOpposite();
+	}
+
+	@Override
+	public boolean isSourceRemoved() {
+		return remove;
+	}
+
+	@Override
+	public void onSpeedChanged(float prevSpeed) {
+		super.onSpeedChanged(prevSpeed);
+		updateAirFlow = true;
+		updateChute();
+	}
+
+	public void updateChute() {
+		Direction direction = getBlockState().getValue(EncasedFanBlock.FACING);
+		if (!direction.getAxis()
+			.isVertical())
+			return;
+		BlockEntity poweredChute = level.getBlockEntity(worldPosition.relative(direction));
+		if (!(poweredChute instanceof ChuteTileEntity))
+			return;
+		ChuteTileEntity chuteTE = (ChuteTileEntity) poweredChute;
+		if (direction == Direction.DOWN)
+			chuteTE.updatePull();
+		else
+			chuteTE.updatePush(1);
+	}
+
+	public void blockInFrontChanged() {
+		updateAirFlow = true;
+	}
+
+	@Override
+	public void tick() {
+		super.tick();
+
+		boolean server = !level.isClientSide || isVirtual();
+
+		if (server && airCurrentUpdateCooldown-- <= 0) {
+			airCurrentUpdateCooldown = AllConfigs.SERVER.kinetics.fanBlockCheckRate.get();
+			updateAirFlow = true;
+		}
+
+		if (updateAirFlow) {
+			updateAirFlow = false;
+			airCurrent.rebuild();
+			if (airCurrent.maxDistance > 0)
+				award(AllAdvancements.ENCASED_FAN);
+			sendData();
+		}
+		if (updateGenerator) {
+			updateGenerator = false;
+			updateGenerator();
+		}
+
+		if (getSpeed() == 0 || isGenerator)
+
+			return;
+
+		if (entitySearchCooldown-- <= 0) {
+			entitySearchCooldown = 5;
+			airCurrent.findEntities();
+		}
+
+		airCurrent.tick();
+	}
+
+}
